71746d40d57398e95eed8a9c3b784ed42db4ad7a
ahenden13 <alanahenden@gmail.com>
2025-09-19 14:58:57 -0700

    wip: save before refactor

--- api-app/jest.config.js
+++ api-app/jest.config.js
@@ -1,13 +1,17 @@
 module.exports = {
   preset: 'react-native',
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
-  },
   setupFilesAfterEnv: [
     '@testing-library/jest-native/extend-expect'
   ],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transform: {
+    '^.+\\.(ts|tsx|js|jsx)$': 'babel-jest',
+  },
   transformIgnorePatterns: [
-    "node_modules/(?!(react-native|@react-native|@react-native-community)/)"
+    'node_modules/(?!(react-native|@react-native|@react-navigation|react-native-reanimated|react-native-gesture-handler|react-native-screens|react-native-safe-area-context|react-native-vector-icons|@expo/vector-icons)/)'
   ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
 };
